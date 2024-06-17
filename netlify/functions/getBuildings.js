c961b1999807b5b41e97d435c9e9122dcda04c02
JDemlow <jdemlowdev@gmail.com>
2024-06-17 17:30:46 -0600

    added netligy functions

---
+++ netlify/functions/getBuildings.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function handler(event, context) {
+  try {
+    if (!client.isConnected()) await client.connect();
+    const db = client.db("your-database-name");
+    const collection = db.collection("your-collection-name");
+    const buildings = await collection.find({}).toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(buildings),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  }
+}
+
+module.exports = { handler };
