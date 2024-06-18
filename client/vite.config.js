e9587fe7b8c6a8c5972ac195f2b3df9ef5aa9956
JDemlow <jdemlowdev@gmail.com>
2024-06-17 18:52:42 -0600

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Determine the API URL based on the environment
+const apiURL =
+  process.env.NODE_ENV === "production"
+    ? "https://https://denvermha.netlify.app/.netlify/functions"
+    : "http://localhost:5000/api";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000/api",
+        target: apiURL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
