4e65ff6528c96bda55739dd6faa6cf67e74d96e7
Hemant187 <hksainveera18@gmail.com>
2024-06-20 20:54:36 +0530

    update req solved

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -40,6 +41,35 @@ app.get("/api/buildings", async (req, res) => {
 app.get("/api/buildings/:id", async (req, res) => {
   try {
     const building = await Building.findById(req.params.id);
+    res.json(building);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/api/buildings", async (req, res) => {
+  try {
+    const building = await new Building({
+      id: req.body.id,
+      buildingName: req.body.buildingName,
+      type: req.body.type,
+      description: req.body.description,
+      location: req.body.location,
+      rent: req.body.rent,
+      company: req.body.company,
+    })
+    await building.save()
+    res.json(building);
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(err);
+  }
+});
+app.put("/api/buildings/:id", async (req, res) => {
+  try {
+    const building = await Building.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     res.json(building);
   } catch (err) {
     res.status(500).send(err);
